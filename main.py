dacab3d17ae2bbdd23b321663babaef700b27d09
tburewicz <86408246+tburewicz@users.noreply.github.com>
2022-02-08 10:50:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+from zipfile import *
+import os
+from datetime import datetime
+
+def createZipFile(zipName,version,updated):
+    zipObject = ZipFile(zipName, 'w')
+    zipObject.write(version)
+    if updated != 0:
+        zipObject.write(updated)
+        os.remove(updated)
+    os.remove(version)
+
+def unzipFile(source,finish):
+    try:
+        with ZipFile(source, 'r') as zip:
+            zip.extractall(finish)
+    except (IOError, BadZipFile) as e:
+        print("File corrupted")
+        exit(1)
+
+def checkExist(name):
+    if os.path.isfile(str(name)):
+        print(str(name) + " already exists.")
+        unzipFile(name, "./")
+        if os.path.isfile("VERSION.txt"):
+            f = open("VERSION.txt", "r")
+            return f.read()
+        else:
+            print("File 'VERSION.txt' deosn't exist, but it will be soon.")
+
+print("Parameters\nSet zip file name:")
+setZipName = input()
+setZipName = setZipName + ".zip"
+
+testFile = checkExist(setZipName)
+if testFile == None:
+    print("Set new file version")
+else:
+    print("\n" + str(testFile) + "\nSet new version:")
+setVersion = input()
+
+print("Do you want to save update date? (y/n)")
+update = input()
+
+with open('VERSION.txt', 'w') as f:
+    f.write("Actual version: " + str(setVersion))
+if update == "y":
+    actualDate = datetime.today().strftime('%Y-%m-%d')
+    with open('updated.txt', 'w') as f:
+        f.write(str(actualDate))
+    update = "updated.txt"
+else:
+    update = 0
+    if os.path.isfile("updated.txt"):
+        #if user dont want to have update file it can be deleted if exists
+        os.remove("updated.txt")
+
+createZipFile(setZipName, 'VERSION.txt', update)
+
+
+
+
+
+
+
